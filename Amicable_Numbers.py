bcd45e9761fa5b84b33ba70a2963596ee1f0ce26
Meghana-devi4188 <83063472+Meghana-devi4188@users.noreply.github.com>
2022-10-14 16:15:39 +0530

    v=int(input()) k=int(input()) a=0 for i in range(1,(v//2)+1,1):     if(v%i==0):         a+=i b=0 for i in range(1,(k//2)+1,1):     if(k%i==0):        b+=i if(a==k and b==v):     print('Amicable') else:     print('Not Amicable')

--- Amicable_Numbers.py
+++ Amicable_Numbers.py
@@ -1,17 +1,14 @@
-x=int(input())
-y=int(input())
-a=1
-b=1
-s1=0
-s2=0
-while a<x and b<y:
-    if x%a==0:
-        s1+=a
-    if y%b==0:
-        s2+=b
-    a+=1
-    b+=1
-if s1==y and s2==x:
-    print("Amicable")
+v=int(input())
+k=int(input())
+a=0
+for i in range(1,(v//2)+1,1):
+    if(v%i==0):
+        a+=i
+b=0
+for i in range(1,(k//2)+1,1):
+    if(k%i==0):
+       b+=i
+if(a==k and b==v):
+    print('Amicable')
 else:
-    print("Not Amicable")
+    print('Not Amicable')
